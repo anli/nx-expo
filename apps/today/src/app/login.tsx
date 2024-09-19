b7eb2697c53d8c6d99ac5cf176424b3e6ac5c6a4
Su Anli <anli@users.noreply.github.com>
2024-09-19 14:39:36 +0800

    feat(apps/today): add keyboard avoid UI (#10)

--- apps/today/src/app/login.tsx
+++ apps/today/src/app/login.tsx
@@ -5,6 +5,7 @@ import { yupResolver } from '@hookform/resolvers/yup';
 import { tw } from '@shared/ui';
 import { Link, router } from 'expo-router';
 import { useForm, Controller } from 'react-hook-form';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-controller';
 import { Button, Text, TextInput } from 'react-native-paper';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import * as yup from 'yup';
@@ -43,106 +44,111 @@ export default function Login() {
 
   return (
     <SafeAreaView style={tw`flex-1`}>
-      <Text variant="displayMedium" style={tw`mx-4`}>
-        Login
-      </Text>
+      <KeyboardAwareScrollView contentContainerStyle={tw`flex-1`}>
+        <View style={tw`flex-1`} />
+        <View>
+          <Text variant="displayMedium" style={tw`mx-4`}>
+            Login
+          </Text>
 
-      <View style={tw`h-4`} />
+          <View style={tw`h-4`} />
 
-      <Controller
-        control={control}
-        rules={{
-          required: true,
-        }}
-        render={({
-          field: { onChange, onBlur, value },
-          fieldState: { error },
-        }) => (
-          <>
-            <TextInput
-              mode="outlined"
-              label="Email"
-              style={tw`mx-4`}
-              autoFocus
-              value={value}
-              onChangeText={onChange}
-              onBlur={onBlur}
-              autoCapitalize="none"
-              autoCorrect={false}
-              autoComplete="email"
-              aria-errormessage="emailError"
-              aria-labelledby="emailLabel"
-            />
-            {error?.message && (
-              <Text
-                variant="labelSmall"
-                style={tw`mx-4 mt-1`}
-                nativeID="emailError"
-              >
-                {error?.message}
-              </Text>
+          <Controller
+            control={control}
+            rules={{
+              required: true,
+            }}
+            render={({
+              field: { onChange, onBlur, value },
+              fieldState: { error },
+            }) => (
+              <>
+                <TextInput
+                  mode="outlined"
+                  label="Email"
+                  style={tw`mx-4`}
+                  autoFocus
+                  value={value}
+                  onChangeText={onChange}
+                  onBlur={onBlur}
+                  autoCapitalize="none"
+                  autoCorrect={false}
+                  autoComplete="email"
+                  aria-errormessage="emailError"
+                  aria-labelledby="emailLabel"
+                />
+                {error?.message && (
+                  <Text
+                    variant="labelSmall"
+                    style={tw`mx-4 mt-1`}
+                    nativeID="emailError"
+                  >
+                    {error?.message}
+                  </Text>
+                )}
+              </>
             )}
-          </>
-        )}
-        name="email"
-      />
+            name="email"
+          />
 
-      <View style={tw`h-2`} />
+          <View style={tw`h-2`} />
 
-      <Controller
-        control={control}
-        rules={{
-          required: true,
-        }}
-        render={({
-          field: { onChange, onBlur, value },
-          fieldState: { error },
-        }) => (
-          <>
-            <TextInput
-              mode="outlined"
-              label="Password"
-              style={tw`mx-4`}
-              value={value}
-              onChangeText={onChange}
-              onBlur={onBlur}
-              autoCapitalize="none"
-              autoCorrect={false}
-              secureTextEntry
-              aria-labelledby="passwordLabel"
-              aria-errormessage="passwordError"
-              autoComplete="password"
-            />
-            {error?.message && (
-              <Text
-                variant="labelSmall"
-                style={tw`mx-4 mt-1`}
-                nativeID="passwordError"
-              >
-                {error?.message}
-              </Text>
+          <Controller
+            control={control}
+            rules={{
+              required: true,
+            }}
+            render={({
+              field: { onChange, onBlur, value },
+              fieldState: { error },
+            }) => (
+              <>
+                <TextInput
+                  mode="outlined"
+                  label="Password"
+                  style={tw`mx-4`}
+                  value={value}
+                  onChangeText={onChange}
+                  onBlur={onBlur}
+                  autoCapitalize="none"
+                  autoCorrect={false}
+                  secureTextEntry
+                  aria-labelledby="passwordLabel"
+                  aria-errormessage="passwordError"
+                  autoComplete="password"
+                />
+                {error?.message && (
+                  <Text
+                    variant="labelSmall"
+                    style={tw`mx-4 mt-1`}
+                    nativeID="passwordError"
+                  >
+                    {error?.message}
+                  </Text>
+                )}
+              </>
             )}
-          </>
-        )}
-        name="password"
-      />
+            name="password"
+          />
 
-      <View style={tw`h-4`} />
+          <View style={tw`h-4`} />
 
-      <Button
-        mode="contained"
-        // eslint-disable-next-line @typescript-eslint/no-misused-promises
-        onPress={handleSubmit(loginWithEmail)}
-        style={tw`mx-4`}
-        loading={loading}
-      >
-        Login
-      </Button>
-      <Link href="/sign-up" asChild>
-        <Button mode="text" style={tw`mx-4`}>
-          Don&apos;t have an account? Sign up
-        </Button>
-      </Link>
+          <Button
+            mode="contained"
+            // eslint-disable-next-line @typescript-eslint/no-misused-promises
+            onPress={handleSubmit(loginWithEmail)}
+            style={tw`mx-4`}
+            loading={loading}
+          >
+            Login
+          </Button>
+          <Link href="/sign-up" asChild>
+            <Button mode="text" style={tw`mx-4`}>
+              Don&apos;t have an account? Sign up
+            </Button>
+          </Link>
+        </View>
+      </KeyboardAwareScrollView>
     </SafeAreaView>
   );
 }
